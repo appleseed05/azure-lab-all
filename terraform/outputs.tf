862b742be30f0d52b02e7e9accebf5f985fc87aa
appleseed05 <appleseed@powermail.fr>
2026-08-21 09:11:10 +0200

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,21 +1,25 @@
+###############################################################################
+# Outputs
+###############################################################################
+
 output "Azure_Resource_Group_name" {
   value = azurerm_resource_group.tf_azure_rg.name
-}
-
-output "Azure_Public_IP_Jumphost_VM" {
-  value = azurerm_public_ip.tf_azure_pip-jmp.ip_address
 }
 
 output "Jumphost_RDP_file" {
   value = local_file.tf_jumphost_rdp.filename
 }
 
-output "Azure_Public_IP_NAT_Gateway" {
-  value = azurerm_public_ip.tf_azure_pip-nat.ip_address
+output "Azure_Public_IP_Router_VM" {
+  value = azurerm_public_ip.tf_azure_pip_rtr.ip_address
 }
 
-output "XC_SMSv2_name" {
-  value = volterra_securemesh_site_v2.tf_f5xc_smsv2-site-name.name
+output "XC_site_name_01" {
+  value = volterra_securemesh_site_v2.tf_f5xc_site_01.name
+}
+
+output "XC_site_name_02" {
+  value = volterra_securemesh_site_v2.tf_f5xc_site_02.name
 }
 
 output "XC_VirtualSite_name" {
